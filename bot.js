d1e2ba5932dcd2305b0b549a29c2f8dcdb66eb45
fhenri42 <fhenri@student.42.fr>
2016-06-14 21:41:00 +0200

    change norme c, to norme JS

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
 var http = require('http');
-var recast = require('recastai')
+var recast = require('recastai');
 var RtmClient = require('@slack/client').RtmClient;
 
 var config = require('./config.js');
 var replyBot = require('./modules/leon-bot');
 var replyGreetings = require('./modules/greetings');
 var replyGoodbyes = require('./modules/goodbyes');
-var replyHelp = require('./modules/help')
+var replyHelp = require('./modules/help');
+
 var token = process.env.SLACK_API_TOKEN || config.slack.Token;
 var rtm = new RtmClient(token, {logLevel: false});
 rtm.start();
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-rtm.on(RTM_EVENTS.MESSAGE, function(message)
-{
-	var user = rtm.dataStore.getUserById(message.user);
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
 
+	var user = rtm.dataStore.getUserById(message.user);
 	if (user != null)
     	var dm = rtm.dataStore.getDMByName(user.name);
 	else
@@ -23,15 +23,14 @@ rtm.on(RTM_EVENTS.MESSAGE, function(message)
 	find_login(message, dm, rtm);
 });
 
-function find_login(message, dm, rtm)
-{
+function find_login (message, dm, rtm) {
+
 	var y = 0;
-	var login;
+	var login = null;
 	var CLIENT = new recast.Client(config.recast.request_token);
-	CLIENT.textRequest(message.text, (res, err) =>
-	{
-		while(res.sentences[0].entities[y]!= null)
-		{
+	CLIENT.textRequest(message.text, (res, err) => {
+
+		while(res.sentences[0].entities[y]!= null){
 			if (res.sentences[0].entities[y].name == '42login')
 				login = res.sentences[0].entities[y].raw;
 			y++;
@@ -40,11 +39,10 @@ function find_login(message, dm, rtm)
 	});
 }
 
-function find_intents(login, res, dm, rtm)
-{
-		var rep;
-		if (login == null)
-		{
+function find_intents (login, res, dm, rtm) {
+
+		var rep = null;
+		if (login == null){
 			if (res.intents[0] == 'goodbyes')
 				rep = replyGoodbyes();
 			else if (res.intents[0] == 'greetings')
@@ -61,7 +59,7 @@ function find_intents(login, res, dm, rtm)
 			replyBot(login, res, dm, rtm);
 }
 
-var server = http.createServer(function(req, res) 
-{
+var server = http.createServer (function (req, res) {
+
 	res.writeHead(200);
 });
